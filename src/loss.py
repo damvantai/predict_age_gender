f82b725182589556d8e8ea9dd34eee0db3d1a1dc
damvantai <tai94bn@gmail.com>
2018-09-11 16:52:42 +0700

    update

--- src/loss.py
+++ src/loss.py
@@ -11,7 +11,11 @@ class AGLoss(nn.Module):
 	def forward(self, age_preds, age_targets, gender_preds, gender_targets):
 		"""Compute loss between (age_preds, age_targets) and (gender_preds, gender_targets)"""
 		age_prob = F.softmax(age_preds, dim=1).cuda()
-		age_expect = torch.sum(Variable(torch.arange(1, 117))*age_prob, 1).cuda()
+		age_expect = torch.sum(Variable(torch.arange(1, 117))*age_prob.long().cpu(), 1).cuda()
+		print(age_prob.shape)
+		print(age_expect.shape)
+		print(gender_preds.shape)
+		print(gender_preds.shape)
 		age_loss = F.smooth_l1_loss(age_expect, age_targets)
 		gender_loss = F.binary_cross_entropy_with_logits(gender_preds.float().cuda(), gender_targets.float().cuda())
 		# print("age_loss: %.3f | gender_loss: %.3f" & (age_loss.data[0], gender_loss.data[0]),
